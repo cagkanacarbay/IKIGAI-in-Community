ca87ca3f40e0352c1012a7b22ddd58a50526dc04
chakhan <cagkanacarbay@gmail.com>
2023-12-04 13:30:24 +0300

    debugging

--- components/gallery/ikigaiCard.tsx
+++ components/gallery/ikigaiCard.tsx
@@ -31,7 +31,7 @@ const IkigaiCard: React.FC<IkigaiCardProps> = ({ ikigaiId, userName, images, tag
 
     // <div>yoyoyoyoy</div>
     <Card className="max-w-sm mx-auto bg-blue-gray-200 hover:bg-blue-gray-300" >
-      <CardHeader
+      {/* <CardHeader
             className="flex items-center justify-center px-6 shadow-md bg-blue-gray-100"
             floated={false}
             shadow={false}
@@ -39,9 +39,9 @@ const IkigaiCard: React.FC<IkigaiCardProps> = ({ ikigaiId, userName, images, tag
           <div className="flex-grow border-t border-gray-700"></div>
           <Typography variant="h5" color="black" className='px-2'>{userName}</Typography>
           <div className="flex-grow border-t border-gray-700"></div>
-      </CardHeader>
-      {/* <CardBody>
-        <Carousel className="rounded-md shadow-md rounded-xl bg-blue-gray-100" onClick={handleCarouselClick}>
+      </CardHeader> */}
+      <CardBody>
+        <Carousel className="rounded-md shadow-md rounded-xl bg-blue-gray-100" >
           {images.map((image, index) => (
             <div key={index} className="h-48 md:h-64 w-full relative rounded-xl "> 
               <img
@@ -49,7 +49,7 @@ const IkigaiCard: React.FC<IkigaiCardProps> = ({ ikigaiId, userName, images, tag
                 src={image}
                 alt={`Ikigai ${index}`}
                 className="h-full w-full object-contain rounded-xl"
-                onClick={handleCarouselClick}
+                // onClick={handleCarouselClick}
               />
             </div>
           ))}
@@ -64,7 +64,7 @@ const IkigaiCard: React.FC<IkigaiCardProps> = ({ ikigaiId, userName, images, tag
             </span>
           ))}
         </div>
-      </CardFooter> */}
+      </CardFooter>
     </Card>
   );
 };
