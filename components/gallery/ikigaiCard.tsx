5bf2f80e43fd0c73eef604676d9b5ec902d0010f
chakhan <cagkanacarbay@gmail.com>
2023-12-04 13:27:44 +0300

    debugging

--- components/gallery/ikigaiCard.tsx
+++ components/gallery/ikigaiCard.tsx
@@ -29,43 +29,43 @@ const IkigaiCard: React.FC<IkigaiCardProps> = ({ ikigaiId, userName, images, tag
 
   return (
 
-    <div>yoyoyoyoy</div>
-    // <Card className="max-w-sm mx-auto bg-blue-gray-200 hover:bg-blue-gray-300" onClick={handleCardClick}>
-    //   <CardHeader
-    //         className="flex items-center justify-center px-6 shadow-md bg-blue-gray-100"
-    //         floated={false}
-    //         shadow={false}
-    //       >
-    //       <div className="flex-grow border-t border-gray-700"></div>
-    //       <Typography variant="h5" color="black" className='px-2'>{userName}</Typography>
-    //       <div className="flex-grow border-t border-gray-700"></div>
-    //   </CardHeader>
-    //   <CardBody>
-    //     <Carousel className="rounded-md shadow-md rounded-xl bg-blue-gray-100" onClick={handleCarouselClick}>
-    //       {images.map((image, index) => (
-    //         <div key={index} className="h-48 md:h-64 w-full relative rounded-xl "> 
-    //           <img
-    //             key={index}
-    //             src={image}
-    //             alt={`Ikigai ${index}`}
-    //             className="h-full w-full object-contain rounded-xl"
-    //             onClick={handleCarouselClick}
-    //           />
-    //         </div>
-    //       ))}
-    //     </Carousel>  
+    // <div>yoyoyoyoy</div>
+    <Card className="max-w-sm mx-auto bg-blue-gray-200 hover:bg-blue-gray-300" >
+      <CardHeader
+            className="flex items-center justify-center px-6 shadow-md bg-blue-gray-100"
+            floated={false}
+            shadow={false}
+          >
+          <div className="flex-grow border-t border-gray-700"></div>
+          <Typography variant="h5" color="black" className='px-2'>{userName}</Typography>
+          <div className="flex-grow border-t border-gray-700"></div>
+      </CardHeader>
+      {/* <CardBody>
+        <Carousel className="rounded-md shadow-md rounded-xl bg-blue-gray-100" onClick={handleCarouselClick}>
+          {images.map((image, index) => (
+            <div key={index} className="h-48 md:h-64 w-full relative rounded-xl "> 
+              <img
+                key={index}
+                src={image}
+                alt={`Ikigai ${index}`}
+                className="h-full w-full object-contain rounded-xl"
+                onClick={handleCarouselClick}
+              />
+            </div>
+          ))}
+        </Carousel>  
 
-    //   </CardBody>
-    //   <CardFooter className='pt-0'>
-    //     <div className="flex flex-wrap justify-center gap-1">
-    //       {tags.map((tag, index) => (
-    //         <span key={index} className="rounded-full shadow-xs px-3 py-1 text-sm text-black bg-white m-1">
-    //           {tag}
-    //         </span>
-    //       ))}
-    //     </div>
-    //   </CardFooter>
-    // </Card>
+      </CardBody>
+      <CardFooter className='pt-0'>
+        <div className="flex flex-wrap justify-center gap-1">
+          {tags.map((tag, index) => (
+            <span key={index} className="rounded-full shadow-xs px-3 py-1 text-sm text-black bg-white m-1">
+              {tag}
+            </span>
+          ))}
+        </div>
+      </CardFooter> */}
+    </Card>
   );
 };
 
