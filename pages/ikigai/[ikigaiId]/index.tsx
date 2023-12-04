b62135156c2d2dff5f097b4aab6dd7c7ea91e1fd
chakhan <cagkanacarbay@gmail.com>
2023-12-04 12:27:06 +0300

    debugging deployment

--- pages/ikigai/[ikigaiId]/index.tsx
+++ pages/ikigai/[ikigaiId]/index.tsx
@@ -2,82 +2,79 @@ import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import IkigaiBoard from '@/components/ikigai/ikigaiBoard';
 import { Spinner } from "@material-tailwind/react";
-import { IkigaiItems, IkigaiItem, Position } from '@/lib/types';
-import { UserInDB } from '@/lib/types';
+import { IkigaiItems, UserInDB } from '@/lib/types';
 
+interface ApiItem {
+  item_id: number;
+  type: 'image' | 'tag';
+  positions: Array<{ position_id: number; x_position: number; y_position: number; }>;
+  text: string;
+  image_url: string;
+}
 
 export default function Ikigai() {
   const [items, setItems] = useState<IkigaiItems>({});
   const [isLoading, setIsLoading] = useState(true); 
   const [user, setUser] = useState<UserInDB>();
   const router = useRouter();
-  const { ikigaiId } = router.query;
-
-  console.log("rendering Ikigai component again for ikigaiId: ", ikigaiId)
+  const ikigaiId = Array.isArray(router.query.ikigaiId) ? router.query.ikigaiId[0] : router.query.ikigaiId;
 
   useEffect(() => {
+    if (!ikigaiId) {
+      setIsLoading(false);
+      return;
+    }
+    setIsLoading(true);
+
     const fetchIkigai = async () => {
-      if (ikigaiId) {
-        setIsLoading(true); 
-        try {
-          const response = await fetch(`/api/ikigai/${ikigaiId}`);
-          if (!response.ok) {
-            throw new Error('Failed to fetch Ikigai');
-          }
-          const data = await response.json();
-          setUser(data.user);
-
-          // Process items to add a position attribute
-          const processedItems: IkigaiItems = {};
-          data.items.forEach((item: any) => {
-            const lastPosition = item.positions.reduce((latest: any, current: any) => 
-              current.position_id > latest.position_id ? current : latest, item.positions[0]);
-
-            const position: Position = {
-              x: lastPosition.x_position,
-              y: lastPosition.y_position,
-            };
-
-            processedItems[item.item_id] = {
-              type: item.type as 'image' | 'tag',
-              position,
-              text: item.text,
-              imageUrl: item.image_url,
-              storageUrl: undefined // Add storageUrl if applicable
-            };
-          });
-          
-          console.log("Here is the data from api/ikigai/ikigaiId")
-          console.log(data)
-          console.log(processedItems);
-          setItems(processedItems);
-        } catch (error) {
-          console.error('Error fetching Ikigai:', error);
-        } finally {
-          setIsLoading(false); 
+      try {
+        const response = await fetch(`/api/ikigai/${ikigaiId}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch Ikigai');
         }
-      } else {
-        setIsLoading(false);
+        const data = await response.json();
+        setUser(data.user);
+        setItems(processItems(data.items));
+      } catch (error) {
+        console.error('Error fetching Ikigai:', error);
+      } finally {
+        setIsLoading(false); 
       }
     };
 
     fetchIkigai();
-  }, []);
-  
+  }, [ikigaiId]); // Only re-run the effect if ikigaiId changes
+
+  const processItems = (items: ApiItem[]) => {
+    const processedItems: IkigaiItems = {};
+    items.forEach((item: any) => {
+      const lastPosition = item.positions.reduce((latest: any, current: any) => 
+        current.position_id > latest.position_id ? current : latest, item.positions[0]);
+
+      processedItems[item.item_id] = {
+        type: item.type as 'image' | 'tag',
+        position: { x: lastPosition.x_position, y: lastPosition.y_position },
+        text: item.text,
+        imageUrl: item.image_url,
+        // Add storageUrl if applicable
+      };
+    });
+
+    return processedItems;
+  };
+
+  if (isLoading) {
+    return (
+      <div className="flex min-h-screen flex-col items-center justify-center">
+        <Spinner className="h-16 w-16 text-gray-900/50" />
+        <p>Ikigai loading...</p>
+      </div>
+    );
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="relative">
-        {isLoading ? (
-            <div className="flex flex-col justify-center items-center">
-            <Spinner className="h-16 w-16 text-gray-900/50" />
-            <br></br>
-            Ikigai loading...
-          </div>
-        ) : (
-          <IkigaiBoard ikigaiItems={items} setIkigaiItems={setItems} ownerUser={user} ikigaiId={Array.isArray(ikigaiId) ? ikigaiId[0] : ikigaiId}/>
-        )}
-      </div>
+      <IkigaiBoard ikigaiItems={items} setIkigaiItems={setItems} ownerUser={user} ikigaiId={ikigaiId} />
     </main>
   );
 }
