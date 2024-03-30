f09eb68a06fbf9c47941af89c22dd0f2e2bddc0a
Cha <cagkanacarbay@gmail.com>
2024-03-30 17:07:29 +0300

    removed older parts from navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -56,16 +56,16 @@ const Navbar = () => {
                     <span className="block text-sm text-gray-500 truncate dark:text-gray-400">{session.user.email}</span>
                   </div>
                   <ul className="py-2" aria-labelledby="user-menu-button">
-                    <li>
-                      <Link href={"/ikigai/me"} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                        My Ikigais
+                    {/* <li>
+                      <Link href={"/board/latest"} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                        Go to latest board
                       </Link>
-                    </li>
-                    <li>
+                    </li> */}
+                    {/* <li>
                       <Link href={"/profile"} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
                         Profile
                       </Link>
-                    </li>
+                    </li> */}
                     <li>
                       <Link href={'/api/auth/signout'} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
                         Sign out
@@ -116,20 +116,20 @@ const Navbar = () => {
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
-                href="/create"
+                href="/board/latest"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                Create
+                Go to latest board
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link
-                href="/gallery"
+                href="/board/new"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                Gallery
+                Create a new board
               </Link>
-            </li>
+            </li> */}
           </ul>
         </div>
       </div>
